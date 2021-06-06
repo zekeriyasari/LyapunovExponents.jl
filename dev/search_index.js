8b8cfe9def4d74872d0cbb919f446dbcaef169b2
Documenter.jl <documenter@juliadocs.github.io>
2021-06-06 19:51:29 +0000

    build based on a2271d2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LyapunovExponents","category":"page"},{"location":"#LyapunovExponents","page":"Home","title":"LyapunovExponents","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LyapunovExponents.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LyapunovExponents]","category":"page"}]
+}
