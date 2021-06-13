f8fb4b6d77532ae5695d189fbc20ef0e9c716f73
zekeriya.sari <zekeriya.sari@deu.edu.tr>
2021-06-13 15:31:50 +0300

    Added notebook

--- example/src/weightsweep.jl
+++ example/src/weightsweep.jl
@@ -13,14 +13,14 @@ using GraphPlot
 
 # Construct the network 
 n = 5   # Number of nodes in the network 
-gr = cycle_graph(n) 
-add_edge!(gr, 1, 3)
-add_edge!(gr, 1, 4)
-add_edge!(gr, 2, 5)
-gplot(gr, nodelabel=1:nv(gr), layout = circular_layout)
+graph = cycle_graph(n) 
+add_edge!(graph, 1, 3)
+add_edge!(graph, 1, 4)
+add_edge!(graph, 2, 5)
+gplot(graph, nodelabel=1:nv(graph), layout = circular_layout)
 
 # The outer connection matrix of the network is 
-W = collect(laplacian_matrix(gr)) .|> float 
+W = collect(laplacian_matrix(graph)) .|> float 
 
 # Let us rescale the matrix such elements on the main diagonal are 1. 
 function scale(W0) 
@@ -104,4 +104,87 @@ vline!([1.], color="red", linestyle=:dash)
 xlabel!("w") 
 
 # Note from the figure that as the link between the node 2 and 5 exceeds the critical value $ w_c = 1 $, the eigenvalue
-# condition is satisfied. 
+# condition $ \lambda_2^{syn} < \lambda_1^{tr} $ is satisfied. 
+
+# To further verify the results, we will simulate the network. For this purpose we choose the Lorenz system defiend as 
+# $$ 
+# f([x, y, z]) = \left\{ 
+# \begin{aligned} 
+#   \dot{x} &= \sigma (y - x) \\
+#   \dot{x} &= x (\rho - z) - y \\
+#   \dot{x} &= x y - \beta z \\
+# \end{aligned} 
+# \right.
+# $$ 
+# For $ \sigma = 10, \beta = 8 / 3, \rho = 35 $, the system is chaotic with the largest lyapunov exponent $
+# \Lambda_{max} ≈ 0.94 $. To verifyt this numerically, let us calculate the lyapunov exponennts of Lorenz system. 
+using LyapunovExponents 
+ds = Lorenz(σ = 10, β = 8 / 3, ρ = 35) 
+nsteps = Int(3e4)   # Number of steps to calculate Lyapunov exponents 
+ntrsteps=Int(1e4)   # Number of steps transient steps before calcuting Lyapunov exponents 
+dt = 0.01           # Integration step size 
+lyaplorenz = lyapunovs(ds, nsteps = Int(3e4), ntrsteps=Int(1e4), dt=0.01) 
+
+# The nodes in the network is connected through their $x$ states. Thus, the innner connection matrix $ P $ is 
+P = [
+    1 0 0;
+    0 0 0;
+    0 0 0
+]
+
+# Let us denote $\eta = \epsilon \lambda $ and plot the master stability fucntion of the network. 
+η = range(0, 100, length=21)
+Ψ(ηi) = msf(ds, ηi, P)
+Λ = Ψ.(η) 
+plot(η, Λ, marker=(:circle, 3)) 
+hline!([0])
+threshold = η[findfirst(Λ .< 0)]
+@info "The critical value of η: $threshold"
+vline!([threshold], label="Threshold: $threshold")
+
+# Note from the plot of the MSF that the critical value $\eta^\star$ after which the the mode changes from instability
+# to stability is $\eta^\star \approx 10 $. Thus, to make the transverse mode $ \lambda_1^{tr} $, it is necessary that
+# the coupling strength $ \epsilon $ should be larger than $ \epsilon_1 = \eta^\star / \lambda_1^{tr}$. In the mean time
+# to avoid full syncronization, we should also keep the coupling strength $ \epsilon $ smaller than $ \epsilon_2 =
+# \eta^\star / \lambda_2^{syn} $. Hence, the region for $ \epsilon $ is $ [\epsilon_1, \epsilon_2]$. For example, if we
+# choose the coupling strength between the nodes 2 and 5 as $ w_i = 1.5 $, we can find the interval for $ \epsilon =
+#  [\epsilon_1, \epsilon_2] = [0.35, 10.82] $
+W = collect(laplacian_matrix(graph)) .|> float 
+wi = 1.5 
+W[2, 5] = W[5, 2] =  -wi 
+W[2, 2] -= sum(W[2, :])
+W[5, 5] -= sum(W[5, :])
+Ξ = scale(W) 
+G = inv(M) * Ξ * M 
+B = G[1 : 2, 1 : 2] 
+D = G[3 : 5, 3 : 5] 
+λB = eigvals(B) 
+λD = eigvals(D) 
+λ1tr = λB[1] 
+λ2syn = λD[2] 
+ϵ1 = threshold / λ1tr
+ϵ2 = threshold / λ2syn
+
+# Let us simulate the system in the nodes by for $ \epsilon = 9.6 $. 
+ϵ = 9.6 
+nodes = [Lorenz(σ = 10, β = 8 / 3, ρ=35) for i in 1 : n]
+net = Network(nodes, -ϵ * Ξ, P)
+sol = solvenet(net, (0., 1000.))
+t, x = sol.t, sol.u 
+
+d = dimension(nodes[1])
+getwave(x, i) = getindex.(x, (i - 1) * d + 1)  
+
+plt = plot(layout = (4, 1))
+plot!(t, abs.(getwave(x, 2) -  getwave(x, 5)), label="e25", subplot=1)
+plot!(t, abs.(getwave(x, 3) -  getwave(x, 4)), label="e34", subplot=2)
+plot!(t, abs.(getwave(x, 1) -  getwave(x, 2)), label="e12", subplot=3)
+plot!(t, abs.(getwave(x, 1) -  getwave(x, 3)), label="e13", subplot=4)
+plt 
+
+# Note that the as time evolves nodes 2 and 5 synchronize, and the nodes 3 and 4 synchronize to each other. So we have
+# the cluster pattern $(a, b, c, c, b)$ where each letter corresponds to a cluster. To we have three clusters, $ G_a =
+# [1], G_b = [2, 5], G_c = [3, 4]$ . 
+
+# #### References 
+# * Fu, C., Deng, Z., Huang, L., & Wang, X. (2013). Topological control of synchronous patterns in systems of networked chaotic oscillators. Physical Review E - Statistical, Nonlinear, and Soft Matter Physics, 87(3), 032909. https://doi.org/10.1103/PhysRevE.87.032909
